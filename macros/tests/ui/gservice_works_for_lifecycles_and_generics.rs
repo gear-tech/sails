180db97217f53ba014af22019c5ea132c2051218
Dennis Diatlov <dennis@gear-tech.io>
2024-01-29 12:27:36 +0000

    Support lifecycles and generics in idlgen

---
+++ macros/tests/ui/gservice_works_for_lifecycles_and_generics.rs
@@ -0,0 +1,36 @@
+use core::marker::PhantomData;
+use parity_scale_codec::{Decode, Encode};
+use sails_macros::gservice;
+use scale_info::TypeInfo;
+
+struct MyService<'a, T> {
+    _a: PhantomData<&'a T>,
+}
+
+#[gservice]
+impl<'a, T> MyService<'a, T>
+where
+    T: Clone,
+{
+    pub fn do_this(&mut self) -> u32 {
+        42
+    }
+}
+
+#[tokio::main]
+async fn main() {
+    const DO_THIS: &str = "DoThis";
+
+    let mut my_service = MyService::<'_, String> { _a: PhantomData };
+
+    let output = requests::process(&mut my_service, &DO_THIS.encode()).await;
+    let mut output = output.as_slice();
+
+    let func_name = String::decode(&mut output).unwrap();
+    assert_eq!(func_name, DO_THIS);
+
+    let result = u32::decode(&mut output).unwrap();
+    assert_eq!(result, 42);
+
+    assert_eq!(output.len(), 0);
+}
