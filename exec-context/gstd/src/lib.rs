e67666ea92200b1b76211846407acb26619b4c24
Dennis Diatlov <dennis@gear-tech.io>
2024-01-29 15:43:56 +0000

    Make clippy happy

--- exec-context/gstd/src/lib.rs
+++ exec-context/gstd/src/lib.rs
@@ -3,6 +3,7 @@
 use gstd::{cell::OnceCell, msg, ActorId};
 use sails_exec_context_abstractions::ExecContext;
 
+#[derive(Default)]
 pub struct GStdExecContext {
     msg_source: OnceCell<ActorId>,
 }
@@ -19,6 +20,6 @@ impl ExecContext for GStdExecContext {
     type ActorId = ActorId;
 
     fn actor_id(&self) -> &ActorId {
-        self.msg_source.get_or_init(|| msg::source())
+        self.msg_source.get_or_init(msg::source)
     }
 }
