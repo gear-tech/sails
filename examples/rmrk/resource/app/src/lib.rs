268e11ac470a4f722eac79df0090b89e04fa4824
Dennis Diatlov <dennis@gear-tech.io>
2024-02-13 18:05:03 +0000

    feat(examples): Implement a regular UT for RMRK Resource service (#93)

--- examples/rmrk/resource/app/src/lib.rs
+++ examples/rmrk/resource/app/src/lib.rs
@@ -114,3 +114,105 @@ fn resource_storage_data_mut() -> &'static mut ResourceStorageData {
 fn resource_storage_admin() -> &'static ActorId {
     unsafe { RESOURCE_STORAGE_ADMIN.as_ref().unwrap() }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use catalogs::{
+        GstdCommonPrimitivesActorId, RmrkCatalogAppErrorsError, RmrkCatalogAppPartsPart,
+    };
+    use gstd::{collections::BTreeMap, result::Result as StdResult};
+    use resources::BasicResource;
+    use sails_sender::Call;
+
+    #[test]
+    fn test_add_resource_entry() {
+        let mut resource_storage =
+            ResourceStorage::new(ExecContextMock { actor_id: 1.into() }, CatalogClientMock);
+        let resource = Resource::Basic(BasicResource {
+            src: "src".to_string(),
+            thumb: None,
+            metadata_uri: "metadata_uri".to_string(),
+        });
+        let (actual_resource_id, actual_resource) = resource_storage
+            .add_resource_entry(1, resource.clone())
+            .unwrap();
+        assert_eq!(actual_resource_id, 1);
+        assert_eq!(actual_resource, resource);
+    }
+
+    struct ExecContextMock {
+        actor_id: ActorId,
+    }
+
+    impl ExecContext for ExecContextMock {
+        type ActorId = ActorId;
+
+        fn actor_id(&self) -> &ActorId {
+            &self.actor_id
+        }
+    }
+
+    struct CatalogClientMock;
+
+    impl CatalogClient for CatalogClientMock {
+        fn add_parts(
+            &mut self,
+            _parts: BTreeMap<u32, RmrkCatalogAppPartsPart>,
+        ) -> Call<StdResult<BTreeMap<u32, RmrkCatalogAppPartsPart>, RmrkCatalogAppErrorsError>>
+        {
+            unimplemented!()
+        }
+
+        fn remove_parts(
+            &mut self,
+            _part_ids: Vec<u32>,
+        ) -> Call<StdResult<Vec<u32>, RmrkCatalogAppErrorsError>> {
+            unimplemented!()
+        }
+
+        fn add_equippables(
+            &mut self,
+            _part_id: u32,
+            _collection_ids: Vec<GstdCommonPrimitivesActorId>,
+        ) -> Call<StdResult<(u32, Vec<GstdCommonPrimitivesActorId>), RmrkCatalogAppErrorsError>>
+        {
+            unimplemented!()
+        }
+
+        fn remove_equippable(
+            &mut self,
+            _part_id: u32,
+            _collection_id: GstdCommonPrimitivesActorId,
+        ) -> Call<StdResult<(u32, GstdCommonPrimitivesActorId), RmrkCatalogAppErrorsError>>
+        {
+            unimplemented!()
+        }
+
+        fn reset_equippables(
+            &mut self,
+            _part_id: u32,
+        ) -> Call<StdResult<(), RmrkCatalogAppErrorsError>> {
+            unimplemented!()
+        }
+
+        fn set_equippables_to_all(
+            &mut self,
+            _part_id: u32,
+        ) -> Call<StdResult<(), RmrkCatalogAppErrorsError>> {
+            unimplemented!()
+        }
+
+        fn part(&self, _part_id: u32) -> Call<Option<RmrkCatalogAppPartsPart>> {
+            unimplemented!()
+        }
+
+        fn equippable(
+            &self,
+            _part_id: u32,
+            _collection_id: GstdCommonPrimitivesActorId,
+        ) -> Call<StdResult<bool, RmrkCatalogAppErrorsError>> {
+            unimplemented!()
+        }
+    }
+}
