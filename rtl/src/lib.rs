a677719f1d4a045e29e08de241030abe5626a805
Dennis Diatlov <dennis@gear-tech.io>
2024-04-12 16:50:01 +0100

    feat(rtl): Introduce implementation of Remoting for GTest (#148)

--- rtl/src/lib.rs
+++ rtl/src/lib.rs
@@ -5,6 +5,8 @@ pub use prelude::*;
 pub mod calls;
 pub mod errors;
 pub mod gstd;
+#[cfg(not(target_arch = "wasm32"))]
+pub mod gtest;
 pub mod prelude;
 pub mod types;
 
@@ -19,4 +21,6 @@ pub mod scale {
 
 pub trait ExecContext {
     fn actor_id(&self) -> &ActorId;
+
+    fn message_id(&self) -> &MessageId;
 }
