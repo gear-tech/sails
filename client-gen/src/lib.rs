1425394aba388f8d9f9500fe642f2ec4f739d4d3
Dennis Diatlov <dennis@gear-tech.io>
2024-04-28 16:44:57 +0100

    Make clippy happy

--- client-gen/src/lib.rs
+++ client-gen/src/lib.rs
@@ -4,7 +4,10 @@ use anyhow::{Context, Result};
 use convert_case::{Case, Casing};
 use std::{ffi::OsStr, fs, path::Path};
 
-pub fn generate_client_from_idl(idl_path: impl AsRef<Path>, out_path: impl AsRef<Path>) -> Result<()> {
+pub fn generate_client_from_idl(
+    idl_path: impl AsRef<Path>,
+    out_path: impl AsRef<Path>
+) -> Result<()> {
     let idl_path = idl_path.as_ref();
     let out_path = out_path.as_ref();
 
