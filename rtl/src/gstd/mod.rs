be4ddf036edb9ed81e4fcf3b9558b6be7716d805
Dennis Diatlov <dennis@gear-tech.io>
2024-04-29 19:29:05 +0100

    fix(events): Prefix emitted events with service invocation route

--- rtl/src/gstd/mod.rs
+++ rtl/src/gstd/mod.rs
@@ -1,10 +1,40 @@
-use crate::{ActorId, ExecContext, MessageId};
+use crate::{collections::BTreeMap, ActorId, ExecContext, MessageId};
 use core::cell::OnceCell;
 pub use gstd::{async_init, async_main, handle_signal, message_loop, msg, record_reply};
 
 pub mod calls;
 pub mod events;
 mod types;
+
+static mut MESSAGE_ID_TO_SERVICE_ROUTE: BTreeMap<MessageId, &'static [u8]> = BTreeMap::new();
+
+pub fn __create_message_scope(service_route: &'static [u8]) -> __MessageScope {
+    let msg_id = current_message_id();
+    unsafe { MESSAGE_ID_TO_SERVICE_ROUTE.insert(msg_id, service_route) };
+    __MessageScope { msg_id }
+}
+
+pub struct __MessageScope {
+    msg_id: MessageId,
+}
+
+impl Drop for __MessageScope {
+    fn drop(&mut self) {
+        let removed_value = unsafe { MESSAGE_ID_TO_SERVICE_ROUTE.remove(&self.msg_id) };
+        if removed_value.is_none() {
+            panic!("Service route not found for message id: {:?}", self.msg_id);
+        }
+    }
+}
+
+fn message_service_route(msg_id: MessageId) -> &'static [u8] {
+    let service_route = unsafe { MESSAGE_ID_TO_SERVICE_ROUTE.get(&msg_id).copied() };
+    service_route.unwrap_or_else(|| panic!("Service route not found for message id: {:?}", msg_id))
+}
+
+fn current_message_id() -> MessageId {
+    msg::id().into()
+}
 
 #[derive(Default)]
 pub struct GStdExecContext {
