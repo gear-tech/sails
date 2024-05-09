5831d518e700f45772342faf10e9ecafc756a465
Dennis Diatlov <dennis@gear-tech.io>
2024-05-09 15:35:10 +0100

    feat(macros): Make program using services via their exposures (#237)

--- examples/this-that-svc/wasm/src/lib.rs
+++ examples/this-that-svc/wasm/src/lib.rs
@@ -1,18 +1,15 @@
 #![no_std]
 
-use core::ptr::addr_of_mut;
 use gstd::msg;
+use sails_rtl::gstd::services::Service;
 use this_that_svc_app::MyService;
-
-static mut MY_SERVICE: MyService = MyService::new();
-
-fn my_service() -> &'static mut MyService {
-    unsafe { &mut *addr_of_mut!(MY_SERVICE) }
-}
 
 #[gstd::async_main]
 async fn main() {
     let input_bytes = msg::load_bytes().expect("Failed to read input");
-    let output_bytes = my_service().handle(&input_bytes).await;
+    let output_bytes = MyService::new()
+        .expose(&[1, 2, 3])
+        .handle(&input_bytes)
+        .await;
     msg::reply_bytes(output_bytes, 0).expect("Failed to send output");
 }
