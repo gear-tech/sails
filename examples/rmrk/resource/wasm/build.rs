83d7be5a903c33a43630f2ecafdc8c45a4dda2f1
Vadim Obradovich <vadik.on@gmail.com>
2024-05-29 16:33:15 +0300

    tests: Make use of generated RmrkResource client in integration tests (#282)

--- examples/rmrk/resource/wasm/build.rs
+++ examples/rmrk/resource/wasm/build.rs
@@ -9,7 +9,11 @@ fn main() {
 
     let idl_file_path = manifest_dir_path.join("rmrk-resource.idl");
 
-    let idl_file = File::create(idl_file_path).unwrap();
+    let idl_file = File::create(idl_file_path.clone()).unwrap();
 
     program::generate_idl::<Program>(idl_file).unwrap();
+
+    let out_dir_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let client_rs_file_path = out_dir_path.join("rmrk_resource.rs");
+    sails_client_gen::generate_client_from_idl(idl_file_path, client_rs_file_path).unwrap();
 }
---
+++ examples/rmrk/resource/wasm/tests/resource_client/mod.rs
@@ -0,0 +1,1 @@
+include!(concat!(env!("OUT_DIR"), "/rmrk_resource.rs"));
