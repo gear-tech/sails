ba9466ebdfe0f255ce324ce6045eb10b0126492a
Timofey <ouxya@pm.me>
2024-06-14 10:29:38 +0000

    move generators to multiple files

---
+++ client-gen/src/helpers.rs
@@ -0,0 +1,46 @@
+use parity_scale_codec::Encode;
+use sails_idl_parser::ast::FuncParam;
+
+pub(crate) fn path_bytes(path: &str) -> (String, usize) {
+    if path.is_empty() {
+        (String::new(), 0)
+    } else {
+        let service_path_bytes = path.encode();
+        let service_path_encoded_length = service_path_bytes.len();
+        let mut service_path_bytes = service_path_bytes
+            .into_iter()
+            .map(|x| x.to_string())
+            .collect::<Vec<_>>()
+            .join(",");
+
+        service_path_bytes.push(',');
+
+        (service_path_bytes, service_path_encoded_length)
+    }
+}
+
+pub(crate) fn method_bytes(fn_name: &str) -> (String, usize) {
+    let route_bytes = fn_name.encode();
+    let route_encoded_length = route_bytes.len();
+    let route_bytes = route_bytes
+        .into_iter()
+        .map(|x| x.to_string())
+        .collect::<Vec<_>>()
+        .join(",");
+
+    (route_bytes, route_encoded_length)
+}
+
+pub(crate) fn encoded_args(params: &[FuncParam]) -> String {
+    if params.len() == 1 {
+        return params[0].name().to_owned();
+    }
+
+    let arg_names = params
+        .iter()
+        .map(|a| a.name())
+        .collect::<Vec<_>>()
+        .join(", ");
+
+    format!("({arg_names})")
+}
--- client-gen/src/lib.rs
+++ client-gen/src/lib.rs
@@ -1,8 +1,16 @@
-pub mod generator;
+mod ctor_generators;
+mod helpers;
+mod io_generators;
+mod root_generator;
+mod service_generators;
+mod type_generators;
+
+use root_generator::RootGenerator;
 
 use anyhow::{Context, Result};
 use convert_case::{Case, Casing};
-use std::{ffi::OsStr, fs, path::Path};
+use sails_idl_parser::ast::{visitor, Program};
+use std::{ffi::OsStr, fs, io::Write, path::Path};
 
 pub fn generate_client_from_idl(
     idl_path: impl AsRef<Path>,
@@ -25,10 +33,54 @@ pub fn generate_client_from_idl(
     let file_name = idl_path.file_stem().unwrap_or(OsStr::new("service"));
     let service_name = file_name.to_string_lossy().to_case(Case::Pascal);
 
-    let buf = generator::generate(program, &service_name).context("failed to generate client")?;
+    let buf = generate(program, &service_name).context("failed to generate client")?;
 
     fs::write(out_path, buf)
         .with_context(|| format!("Failed to write generated client to {}", out_path.display()))?;
 
     Ok(())
+}
+
+pub fn generate(program: Program, default_service_name: &str) -> Result<String> {
+    let mut generator = RootGenerator::new(default_service_name);
+    visitor::accept_program(&program, &mut generator);
+
+    let code = generator.finalize();
+
+    // Check for parsing errors
+    let code = pretty_with_rustfmt(&code);
+
+    Ok(code)
+}
+
+// not using prettyplease since it's bad at reporting syntax errors and also removes comments
+// TODO(holykol): Fallback if rustfmt is not in PATH would be nice
+fn pretty_with_rustfmt(code: &str) -> String {
+    use std::process::Command;
+    let mut child = Command::new("rustfmt")
+        .arg("--config")
+        .arg("format_strings=false")
+        .stdin(std::process::Stdio::piped())
+        .stdout(std::process::Stdio::piped())
+        .spawn()
+        .expect("Failed to spawn rustfmt");
+
+    let child_stdin = child.stdin.as_mut().expect("Failed to open stdin");
+    child_stdin
+        .write_all(code.as_bytes())
+        .expect("Failed to write to rustfmt");
+
+    let output = child
+        .wait_with_output()
+        .expect("Failed to wait for rustfmt");
+
+    if !output.status.success() {
+        panic!(
+            "rustfmt failed with status: {}\n{}",
+            output.status,
+            String::from_utf8(output.stderr).expect("Failed to read rustfmt stderr")
+        );
+    }
+
+    String::from_utf8(output.stdout).expect("Failed to read rustfmt output")
 }
