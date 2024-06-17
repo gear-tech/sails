4e2b6c25d9a945ab92efe44fbf2ba3fd6690e85a
vobradovich <vadik.on@gmail.com>
2024-06-17 21:21:09 +0200

    test this_that_svc nonzero

--- examples/this-that-svc/app/src/lib.rs
+++ examples/this-that-svc/app/src/lib.rs
@@ -1,72 +1,19 @@
 #![no_std]
 
-use gstd::{debug, prelude::*};
-use sails_rtl::{gstd::gservice, H256, U256};
+use sails_rtl::gstd::gprogram;
+
+pub mod service;
 
 #[derive(Default)]
-pub struct MyService;
+pub struct Program;
 
-#[gservice]
-impl MyService {
-    pub const fn new() -> Self {
+#[gprogram]
+impl Program {
+    pub fn new() -> Self {
         Self
     }
 
-    pub async fn do_this(
-        &mut self,
-        p1: u32,
-        p2: String,
-        p3: (Option<String>, u8),
-        p4: TupleStruct,
-    ) -> (String, u32) {
-        debug!("Handling 'do_this': {}, {}, {:?}, {:?}", p1, p2, p3, p4);
-        (p2, p1)
+    pub fn service(&self) -> service::MyService {
+        service::MyService::new()
     }
-
-    pub fn do_that(&mut self, param: DoThatParam) -> Result<(String, u32), (String,)> {
-        debug!("Handling 'do_that': {:?}", param);
-        Ok((param.p2, param.p1))
-    }
-
-    pub fn noop(&mut self) {
-        debug!("Handling 'noop'");
-    }
-
-    pub fn this(&self) -> u32 {
-        debug!("Handling 'this'");
-        42
-    }
-
-    // That
-    pub fn that(&self) -> Result<String, String> {
-        debug!("Handling 'that'");
-        Ok("Forty two".into())
-    }
-}
-
-#[allow(dead_code)]
-#[derive(Debug, Decode, TypeInfo)]
-#[codec(crate = sails_rtl::scale_codec)]
-#[scale_info(crate = sails_rtl::scale_info)]
-pub struct TupleStruct(bool);
-
-#[derive(Debug, Decode, TypeInfo)]
-#[codec(crate = sails_rtl::scale_codec)]
-#[scale_info(crate = sails_rtl::scale_info)]
-pub struct DoThatParam {
-    pub p1: u32,
-    pub p2: String,
-    pub p3: ManyVariants,
-}
-
-#[derive(Debug, Decode, TypeInfo)]
-#[codec(crate = sails_rtl::scale_codec)]
-#[scale_info(crate = sails_rtl::scale_info)]
-pub enum ManyVariants {
-    One,
-    Two(u32),
-    Three(Option<U256>),
-    Four { a: u32, b: Option<u16> },
-    Five(String, H256),
-    Six((u32,)),
 }
--- examples/this-that-svc/wasm/build.rs
+++ examples/this-that-svc/wasm/build.rs
@@ -1,6 +1,6 @@
-use sails_idl_gen::service;
+use sails_idl_gen::program;
 use std::{env, fs::File, path::PathBuf};
-use this_that_svc_app::MyService;
+use this_that_svc_app::Program;
 
 fn main() {
     gwasm_builder::build();
@@ -9,7 +9,11 @@ fn main() {
 
     let idl_file_path = manifest_dir_path.join("this-that-svc.idl");
 
-    let idl_file = File::create(idl_file_path).unwrap();
+    let idl_file = File::create(idl_file_path.clone()).unwrap();
 
-    service::generate_idl::<MyService>(idl_file).unwrap();
+    program::generate_idl::<Program>(idl_file).unwrap();
+
+    let out_dir_path = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let client_rs_file_path = out_dir_path.join("this_that_svc_client.rs");
+    sails_client_gen::generate_client_from_idl(idl_file_path, client_rs_file_path).unwrap();
 }
