dad79154eb3d9a713eae672904d203dd34bc2954
vobradovich <vadik.on@gmail.com>
2024-07-04 12:04:45 +0200

    wip: revert examples

--- examples/this-that-svc/wasm/build.rs
+++ examples/this-that-svc/wasm/build.rs
@@ -9,11 +9,7 @@ fn main() {
 
     let idl_file_path = manifest_dir_path.join("this-that-svc.idl");
 
-    let idl_file = File::create(idl_file_path.clone()).unwrap();
+    let idl_file = File::create(idl_file_path).unwrap();
 
     service::generate_idl::<MyService>(idl_file).unwrap();
-
-    let out_dir_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    let client_rs_file_path = out_dir_path.join("this_that_svc_client.rs");
-    sails_client_gen::generate_client_from_idl(idl_file_path, client_rs_file_path).unwrap();
 }
