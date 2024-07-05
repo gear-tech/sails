9cf3eafb1de87d497a7220e11b3343112a71570d
vobradovich <vadik.on@gmail.com>
2024-07-05 12:27:32 +0200

    ci & tests

--- examples/demo/wasm/tests/gclient_demo.rs
+++ examples/demo/wasm/tests/gclient_demo.rs
@@ -4,25 +4,17 @@ use sails_rtl::{
     calls::*,
     errors::RtlError,
     gsdk::calls::{GSdkArgs, GSdkRemoting},
-    CodeId,
 };
-use tokio::sync::OnceCell;
 
 const DEMO_WASM_PATH: &str = "../../../target/wasm32-unknown-unknown/debug/demo.opt.wasm";
-static ONCE: OnceCell<CodeId> = OnceCell::const_new();
-
-async fn demo_code_id(remoting: GSdkRemoting) -> &'static CodeId {
-    ONCE.get_or_init(|| async { remoting.upload_code_by_path(DEMO_WASM_PATH).await })
-        .await
-}
 
 #[tokio::test]
 #[ignore = "requires running gear node"]
 async fn gclient_counter_works() {
-    let api = GearApi::dev().await.unwrap();
+    let api = GearApi::dev_from_path(env!("GEAR_PATH")).await.unwrap();
     let gas_limit = api.block_gas_limit().unwrap();
     let remoting = GSdkRemoting::new(api);
-    let &code_id = demo_code_id(remoting.clone()).await;
+    let code_id = remoting.upload_code_by_path(DEMO_WASM_PATH).await;
 
     let factory = demo_client::DemoFactory::new(remoting.clone());
     let program_id = factory
@@ -46,9 +38,9 @@ async fn gclient_counter_works() {
 #[tokio::test]
 #[ignore = "requires running gear node"]
 async fn gclient_counter_not_enough_gas() {
-    let api = GearApi::dev().await.unwrap();
+    let api = GearApi::dev_from_path(env!("GEAR_PATH")).await.unwrap();
     let remoting = GSdkRemoting::new(api);
-    let &code_id = demo_code_id(remoting.clone()).await;
+    let code_id = remoting.upload_code_by_path(DEMO_WASM_PATH).await;
 
     let factory = demo_client::DemoFactory::new(remoting.clone());
     let activation_reply = factory.default().send_recv(code_id, "123").await;
