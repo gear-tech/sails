547935a928307bbb75edf96aa0ce71d5e86c7010
vobradovich <vadik.on@gmail.com>
2024-07-10 16:32:00 +0200

    fix merge 2

--- rtl/src/gstd/events.rs
+++ rtl/src/gstd/events.rs
@@ -64,7 +64,7 @@ where
     Ok([prefix, &encoded_event_name[..], &payload[1..]].concat())
 }
 
-pub fn encoded_event_names<TEvents>() -> Result<&'static [Vec<u8>], RtlError>
+fn encoded_event_names<TEvents>() -> Result<&'static [Vec<u8>], RtlError>
 where
     TEvents: StaticTypeInfo,
 {
