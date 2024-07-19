97291f624aa1a21e21c99761d726d00e070464b5
vobradovich <vadik.on@gmail.com>
2024-07-19 16:23:24 +0200

    add proxy test w/ mocks

--- examples/proxy/src/this_that/mod.rs
+++ examples/proxy/src/this_that/mod.rs
@@ -19,3 +19,33 @@ where
         self.this_that.this().recv(this_that_addr).await.unwrap()
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use demo_client::mockall::MockThisThat;
+    use sails::mockall::*;
+
+    #[tokio::test]
+    async fn this_that_caller_works() {
+        // arrange
+        const ACTOR_ID: u64 = 11;
+        let mut mock_this_that = MockThisThat::<()>::new();
+        mock_this_that.expect_this().returning(|| {
+            let mut mock_this_query = MockQuery::new();
+            mock_this_query
+                .expect_recv()
+                .with(predicate::eq(ActorId::from(ACTOR_ID)))
+                .times(1)
+                .returning(move |_| Ok(42));
+            mock_this_query
+        });
+
+        // act
+        let mut this_that_caller = ThisThatCaller::new(mock_this_that);
+        let resp = this_that_caller.call_this(ACTOR_ID.into()).await;
+
+        // assert
+        assert_eq!(42, resp);
+    }
+}
