fcb514cadb8b6dc681ee2b7799d32ca41fda3e81
Dennis Diatlov <dennis@gear-tech.io>
2024-08-06 09:58:13 +0100

    chore: add 'gear-wasm-builder' into 'sails-rs' as an optional dep with re-export

--- examples/rmrk/catalog/wasm/build.rs
+++ examples/rmrk/catalog/wasm/build.rs
@@ -2,7 +2,7 @@ use rmrk_catalog_app::Program;
 use std::{env, path::PathBuf};
 
 fn main() {
-    gwasm_builder::build();
+    sails_rs::build_wasm();
 
     sails_idl_gen::generate_idl_to_file::<Program>(
         PathBuf::from(env::var("CARGO_MANIFEST_DIR").unwrap()).join("rmrk-catalog.idl"),
