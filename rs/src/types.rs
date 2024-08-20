d22cbdc60e823e9daea9ad5ba84344794bb5c73e
Dennis Diatlov <dennis@gear-tech.io>
2024-08-20 10:34:28 +0100

    feat: introduce feature flags 'gstd', 'gtest', 'gclient' for 'sails-rs' (#476)

--- rs/src/types.rs
+++ rs/src/types.rs
@@ -1,6 +1,5 @@
 pub use core::num::{NonZeroU128, NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU8};
-pub use gprimitives::{ActorId, CodeId, MessageId, NonZeroU256};
-pub use primitive_types::*;
+pub use gprimitives::{ActorId, CodeId, MessageId, NonZeroU256, H160, H256, U256};
 
 pub type ValueUnit = u128;
 
