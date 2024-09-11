4352f6f217e84c597bd8b516db8909df95fca704
Nikita Yutanov <nikitayutanov@gmail.com>
2024-09-11 20:06:15 +0300

    Add useSendTransaction hooks gen

--- js/cli/src/generate/hooks-gen.ts
+++ js/cli/src/generate/hooks-gen.ts
@@ -13,13 +13,33 @@ export class HooksGenerator extends BaseGenerator {
   };
 
   public generate() {
-    const libFileName = 'lib'; // TODO: pass file name
+    const LIB_FILE_NAME = 'lib'; // TODO: pass file name
 
     this._out
       .import('@gear-js/api', 'HexString')
-      .import('@gear-js/react-hooks', 'useProgram as useGearProgram')
-      .import(`./${libFileName}`, 'Program')
+      .import('@gear-js/react-hooks', 'useProgram as useGearProgram, useSendProgramTransaction')
+      .import(`./${LIB_FILE_NAME}`, 'Program')
       .block(`export function useProgram(id: HexString | undefined)`, this.generateUseProgramReturn)
       .line();
+
+    Object.values(this._program.services).forEach(({ funcs, ...service }) => {
+      funcs.forEach((func) => {
+        if (func.isQuery) return;
+
+        this._out
+          .block(
+            `export function useSend${service.name}${func.name}Transaction(programId: HexString | undefined)`,
+            () => {
+              this._out
+                .line('const { data: program } = useProgram(programId)')
+                .line()
+                .line(
+                  `return useSendProgramTransaction({ program, serviceName: '${service.name}', functionName: '${func.name}' })`,
+                );
+            },
+          )
+          .line();
+      });
+    });
   }
 }
