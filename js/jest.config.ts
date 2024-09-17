419631526a2f385456cbb398f6025bfc3b2cde93
Nikita Yutanov <nikitayutanov@gmail.com>
2024-09-18 01:58:42 +0300

    Drop redudant jest config props

--- js/jest.config.ts
+++ js/jest.config.ts
@@ -1,19 +1,15 @@
 import type { Config } from '@jest/types';
+import { createDefaultEsmPreset } from 'ts-jest';
 
 const config: Config.InitialOptions = {
+  ...createDefaultEsmPreset(),
   globalSetup: './test/setup.js',
   clearMocks: true,
   coverageProvider: 'v8',
   testEnvironment: 'node',
   verbose: true,
-  preset: 'ts-jest/presets/js-with-babel',
-  transformIgnorePatterns: ['node_modules/(?!@polkadot)/'],
-  extensionsToTreatAsEsm: ['.ts'],
   moduleNameMapper: {
     '^(\\.{1,2}/.*)\\.js$': '$1',
-  },
-  transform: {
-    '^.+\\.tsx?$': ['ts-jest', { useESM: true }],
   },
   testTimeout: 15_000,
 };
