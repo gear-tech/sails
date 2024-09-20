41c1e20d1f65a4dc345e3853847fa7a22c5a472e
Vadim Obradovich <vadik.on@gmail.com>
2024-09-20 18:11:11 +0200

    feat(sails): Allows to return value with reply from a service (#522)

--- rs/macros/core/src/shared.rs
+++ rs/macros/core/src/shared.rs
@@ -6,7 +6,7 @@ use std::collections::BTreeMap;
 use syn::{
     punctuated::Punctuated, spanned::Spanned, FnArg, GenericArgument, Ident, ImplItem, ImplItemFn,
     ItemImpl, Lifetime, Pat, Path, PathArguments, PathSegment, Receiver, ReturnType, Signature,
-    Token, Type, TypePath, TypeReference, TypeTuple, WhereClause,
+    Token, Type, TypeImplTrait, TypeParamBound, TypePath, TypeReference, TypeTuple, WhereClause,
 };
 
 pub(crate) fn impl_type(item_impl: &ItemImpl) -> (TypePath, PathArguments) {
@@ -222,4 +222,49 @@ fn replace_lifetime_with_static_in_path_args(path_args: PathArguments) -> PathAr
     } else {
         path_args
     }
+}
+
+/// Check if type is `CommandReply<T>` and extract inner type `T`
+pub(crate) fn extract_reply_type_with_value(ty: &Type) -> Option<Type> {
+    match ty {
+        Type::Path(tp) => extract_reply_result_type(tp),
+        Type::ImplTrait(imp) => extract_reply_result_type_from_impl_into(imp),
+        _ => None,
+    }
+}
+
+/// Extract `T` type from `CommandReply<T>`
+fn extract_reply_result_type(tp: &TypePath) -> Option<Type> {
+    if let Some(last) = tp.path.segments.last() {
+        if last.ident != "CommandReply" {
+            return None;
+        }
+        if let PathArguments::AngleBracketed(args) = &last.arguments {
+            if args.args.len() == 1 {
+                if let Some(GenericArgument::Type(ty)) = args.args.first() {
+                    return Some(ty.clone());
+                }
+            }
+        }
+    }
+    None
+}
+
+/// Extract `T` type from `impl Into<CommandReply<T>>`
+fn extract_reply_result_type_from_impl_into(tit: &TypeImplTrait) -> Option<Type> {
+    if let Some(TypeParamBound::Trait(tr)) = tit.bounds.first() {
+        if let Some(last) = tr.path.segments.last() {
+            if last.ident != "Into" {
+                return None;
+            }
+            if let PathArguments::AngleBracketed(args) = &last.arguments {
+                if args.args.len() == 1 {
+                    if let Some(GenericArgument::Type(Type::Path(tp))) = args.args.first() {
+                        return extract_reply_result_type(tp);
+                    }
+                }
+            }
+        }
+    }
+    None
 }
