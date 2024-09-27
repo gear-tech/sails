43eaa0fdb689a1421b257ee25644b0d7a78f75f2
vobradovich <vadik.on@gmail.com>
2024-09-27 12:57:55 +0200

    sails-cli client-rs command

--- rs/cli/src/main.rs
+++ rs/cli/src/main.rs
@@ -1,5 +1,7 @@
 use clap::{Parser, Subcommand};
 use sails_cli::program::ProgramGenerator;
+use sails_client_gen::ClientGenerator;
+use std::{path::PathBuf, str::FromStr};
 
 #[derive(Parser)]
 #[command(bin_name = "cargo")]
@@ -25,6 +27,22 @@ enum SailsCommands {
         #[arg(long, help = "Disable generation of program tests using 'gtest'")]
         no_gtest: bool,
     },
+
+    /// Generate client code from IDL
+    #[command(name = "client-rs")]
+    ClientRs {
+        #[arg(help = "Path to the IDL file", value_parser(PathBuf::from_str))]
+        idl_path: PathBuf,
+        #[arg(
+            help = "Path to the output Rust client file",
+            value_parser(PathBuf::from_str)
+        )]
+        out_path: Option<PathBuf>,
+        #[arg(long, help = "Generate module with mocks with specified feature name")]
+        mocks: Option<String>,
+        #[arg(long, help = "Custom path to the sails-rs crate")]
+        sails_crate: Option<String>,
+    },
 }
 
 fn main() -> Result<(), i32> {
@@ -42,6 +60,22 @@ fn main() -> Result<(), i32> {
                 .with_client(!no_client)
                 .with_gtest(!no_gtest);
             program_generator.generate()
+        }
+        SailsCommands::ClientRs {
+            idl_path,
+            out_path,
+            mocks,
+            sails_crate,
+        } => {
+            let mut client_gen = ClientGenerator::from_idl_path(idl_path.as_ref());
+            if let Some(mocks) = mocks.as_ref() {
+                client_gen = client_gen.with_mocks(mocks);
+            }
+            if let Some(sails_crate) = sails_crate.as_ref() {
+                client_gen = client_gen.with_sails_crate(sails_crate);
+            }
+            let out_path = out_path.unwrap_or_else(|| idl_path.with_extension("rs"));
+            client_gen.generate_to(out_path)
         }
     };
 
