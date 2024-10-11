44e710c65f4dde5cc37e36909789d4cd7f7c7b9b
vobradovich <vadik.on@gmail.com>
2024-10-11 13:32:33 +0200

    wip: idl gen

--- rs/cli/src/idlgen.rs
+++ rs/cli/src/idlgen.rs
@@ -1,29 +1,30 @@
 use std::{
     fs,
     path::{Path, PathBuf},
-    process::{exit, Command, ExitStatus},
+    process::{Command, ExitStatus},
 };
 
 use anyhow::Context;
-use cargo_metadata::Package;
+use cargo_metadata::{camino::*, Package};
 
 pub struct CrateIdlGenerator {
-    manifest_path: PathBuf,
+    manifest_path: Utf8PathBuf,
     target_dir: Option<PathBuf>,
 }
 
 impl CrateIdlGenerator {
     pub fn new(manifest_path: PathBuf, target_dir: Option<PathBuf>) -> Self {
         Self {
-            manifest_path,
+            manifest_path: Utf8PathBuf::from_path_buf(manifest_path).unwrap(),
             target_dir,
         }
     }
 
     pub fn generate(self) -> anyhow::Result<()> {
+        println!("...reading metadata: {}", &self.manifest_path);
+        // Get metadata with deps
         let metadata = cargo_metadata::MetadataCommand::new()
             .manifest_path(&self.manifest_path)
-            //.no_deps()
             .exec()?;
 
         let sails_package = metadata
@@ -35,26 +36,65 @@ impl CrateIdlGenerator {
         //self.cargo_doc()
         // print!("{:?}", sails_package);
 
-        let program_package = metadata
-            .packages
-            .iter()
-            .find(|p| &p.id == metadata.workspace_default_members.first().unwrap())
-            .unwrap();
+        let program_package = metadata.root_package().unwrap();
         // print!("{:?}", program_package);
+
+        let target_dir = self
+            .target_dir
+            .map(|p| p.canonicalize().ok())
+            .flatten()
+            .map(Utf8PathBuf::from_path_buf)
+            .map(|t| t.ok())
+            .flatten()
+            .unwrap_or_else(|| metadata.target_directory.clone());
+
+        _ = cargo_doc(&self.manifest_path, &target_dir)?;
+        let docs_path = target_dir
+            .join("doc")
+            .join(format!("{}.json", program_package.name));
+
+        println!("...reading docs: {:?}", docs_path);
+        let json_string = std::fs::read_to_string(docs_path)?;
+        let doc_crate: rustdoc_types::Crate = serde_json::from_str(&json_string)?;
+        let program_meta = doc_crate
+            .paths
+            .iter()
+            .find(|p| p.1.path == vec!["sails_idl_meta", "ProgramMeta"])
+            .context("failed to find sails_rs::ProgramMeta implemetation")?;
+        // println!("{:?}", program_meta);
+
+        let program_struct_path = doc_crate.index.iter().filter_map(|idx| {
+            if let rustdoc_types::ItemEnum::Impl(item) = &idx.1.inner {
+                if let Some(tp) = &item.trait_ {
+                    if &tp.id == program_meta.0 {
+                        if let rustdoc_types::Type::ResolvedPath(path) = &item.for_ {
+                            Some(path)
+                        } else {
+                            None
+                        }
+                    } else {
+                        None
+                    }
+                } else {
+                    None
+                }
+            } else {
+                None
+            }
+        });
+        println!("{:?}", program_struct_path);
+
         let crate_name = get_crate_name(program_package);
-        let workspace_root = metadata.workspace_root;
+        let workspace_root = &metadata.workspace_root;
 
         let crate_path = workspace_root.join(&crate_name);
         let src_path = crate_path.join("src");
         fs::create_dir_all(&src_path)?;
 
-        let manifest_path = crate_path.join("Cargo.toml");
-        write_file(&manifest_path, gen_toml(program_package, sails_package))?;
+        let gen_manifest_path = crate_path.join("Cargo.toml");
+        write_file(&gen_manifest_path, gen_toml(program_package, sails_package))?;
         let main_rs_path = src_path.join("main.rs");
 
-        let target_dir = self
-            .target_dir
-            .unwrap_or_else(|| metadata.target_directory.clone().into_std_path_buf());
         let out_file = target_dir.join(format!("{}.idl", program_package.name));
         write_file(
             main_rs_path,
@@ -63,42 +103,13 @@ impl CrateIdlGenerator {
 
         workspace_members_add(&workspace_root.as_std_path(), &crate_name)?;
 
-        _ = cargo_run_bin(&manifest_path, &crate_name)?;
+        _ = cargo_run_bin(&gen_manifest_path, &crate_name)?;
 
         workspace_members_remove(&workspace_root.as_std_path(), &crate_name)?;
 
         fs::remove_dir_all(crate_path)?;
 
         Ok(())
-    }
-
-    fn cargo_doc(&self) -> anyhow::Result<()> {
-        let cargo_path = std::env::var("CARGO").unwrap_or("cargo".into());
-
-        let mut cmd = Command::new(cargo_path);
-        cmd.env("RUSTC_BOOTSTRAP", "1")
-            .env(
-                "RUSTDOCFLAGS",
-                "-Z unstable-options --document-private-items --document-hidden-items --output-format=json --cap-lints=allow",
-            )
-            .stdout(std::process::Stdio::null()) // Don't pollute output
-            .arg("doc")
-            .arg("--manifest-path")
-            .arg(self.manifest_path.to_str().unwrap())
-            .arg("--no-deps");
-
-        let status = match cmd
-            .status()
-            .context("Failed to execute `cargo doc` command")
-        {
-            Ok(status) => status,
-            Err(e) => {
-                // let _ = display_error(&e, &mut shell);
-                exit(1);
-            }
-        };
-
-        exit(status.code().unwrap_or(1));
     }
 }
 
@@ -143,7 +154,7 @@ fn gen_toml(program_package: &Package, sails_package: &Package) -> String {
     manifest.to_string()
 }
 
-fn gen_main_rs(program_struct_path: &str, out_file: &Path) -> String {
+fn gen_main_rs(program_struct_path: &str, out_file: &cargo_metadata::camino::Utf8Path) -> String {
     format!(
         "
 fn main() {{
@@ -153,7 +164,7 @@ fn main() {{
     .unwrap();
 }}",
         program_struct_path,
-        out_file.to_str().unwrap(),
+        out_file.as_str(),
     )
 }
 
@@ -164,7 +175,7 @@ fn write_file<P: AsRef<Path>, C: AsRef<[u8]>>(path: P, contents: C) -> anyhow::R
 }
 
 fn workspace_members_add(path: &Path, name: &str) -> anyhow::Result<()> {
-    println!("adding member to workspace: {:?}", name);
+    println!("...adding member to workspace: {:?}", name);
 
     let workspace_cargo_toml = path.join("Cargo.toml");
     let toml = fs::read_to_string(&workspace_cargo_toml).context("failed to read Cargo.toml")?;
@@ -178,7 +189,7 @@ fn workspace_members_add(path: &Path, name: &str) -> anyhow::Result<()> {
 }
 
 fn workspace_members_remove(path: &Path, name: &str) -> anyhow::Result<()> {
-    println!("removing member from workspace: {:?}", name);
+    println!("...removing member from workspace: {:?}", name);
 
     let workspace_cargo_toml = path.join("Cargo.toml");
     let toml = fs::read_to_string(&workspace_cargo_toml).context("failed to read Cargo.toml")?;
@@ -200,6 +211,30 @@ fn workspace_members_remove(path: &Path, name: &str) -> anyhow::Result<()> {
     }
 }
 
+fn cargo_doc(
+    manifest_path: &cargo_metadata::camino::Utf8Path,
+    target_dir: &cargo_metadata::camino::Utf8Path,
+) -> anyhow::Result<ExitStatus> {
+    let cargo_path = std::env::var("CARGO").unwrap_or("cargo".into());
+
+    let mut cmd = Command::new(cargo_path);
+    cmd.env("RUSTC_BOOTSTRAP", "1")
+        .env(
+            "RUSTDOCFLAGS",
+            "-Z unstable-options --output-format=json --cap-lints=allow",
+        )
+        .stdout(std::process::Stdio::null()) // Don't pollute output
+        .arg("doc")
+        .arg("--manifest-path")
+        .arg(manifest_path.as_str())
+        .arg("--target-dir")
+        .arg(target_dir.as_str())
+        .arg("--no-deps");
+
+    cmd.status()
+        .context("Failed to execute `cargo doc` command")
+}
+
 fn cargo_run_bin(
     manifest_path: &cargo_metadata::camino::Utf8Path,
     bin_name: &str,
@@ -214,6 +249,7 @@ fn cargo_run_bin(
         bin_name,
     ];
     let mut cmd = Command::new(cargo_path);
-    cmd.args(args);
+    cmd.stdout(std::process::Stdio::null()) // Don't pollute output
+        .args(args);
     cmd.status().context("Failed to execute `cargo` command")
 }
