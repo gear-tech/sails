56468281bd6cd33109a2aebd9694e5904dc747a5
Dennis Dyatlov <dennis@approvalmax.com>
2024-10-24 12:02:54 +0100

    chore(net): add some extension methods to Substrate.Gear.Client

---
+++ net/src/Substrate.Gear.Client/Model/Types/AccountExtensions.cs
@@ -0,0 +1,16 @@
+﻿using EnsureThat;
+using Substrate.NET.Schnorrkel;
+using Substrate.NetApi.Model.Types;
+
+namespace Substrate.Gear.Client.Model.Types;
+
+public static class AccountExtensions
+{
+    public static PublicKey GetPublicKey(this Account account)
+    {
+        EnsureArg.IsNotNull(account, nameof(account));
+        EnsureArg.HasItems(account.Bytes, nameof(account.Bytes));
+
+        return new PublicKey(account.Bytes);
+    }
+}
