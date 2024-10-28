d735b79937b3714163cfc746649a51d794ba57a9
Dennis Diatlov <dennis@gear-tech.io>
2024-10-28 12:01:49 +0000

    feat(net): implement IRemoting via SubstrateClient (#610)

---
+++ net/src/Sails.Remoting/INodeClientProvider.cs
@@ -0,0 +1,15 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Substrate.Gear.Api.Generated;
+
+namespace Sails.Remoting;
+
+internal interface INodeClientProvider
+{
+    /// <summary>
+    /// Returns connected node client.
+    /// </summary>
+    /// <param name="cancellationToken"></param>
+    /// <returns></returns>
+    Task<SubstrateClientExt> GetNodeClientAsync(CancellationToken cancellationToken);
+}
---
+++ net/src/Substrate.Gear.Client/Model/Rpc/BlockExtensions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using EnsureThat;
+using Substrate.Gear.Client.Model.Extrinsics;
+using Substrate.Gear.Client.Model.Types.Base;
+using Substrate.NetApi.Model.Rpc;
+using Substrate.NetApi.Model.Types.Base;
+
+namespace Substrate.Gear.Client.Model.Rpc;
+
+public static class BlockExtensions
+{
+    /// <summary>
+    /// Returns the index of the extrinsic in the block.
+    /// </summary>
+    /// <param name="block"></param>
+    /// <param name="extrinsicHash"></param>
+    /// <returns></returns>
+    /// <exception cref="Exception"></exception>
+    public static uint GetExtrinsicIdxByHash(this Block block, Hash extrinsicHash)
+    {
+        EnsureArg.IsNotNull(block, nameof(block));
+        EnsureArg.IsNotNull(extrinsicHash, nameof(extrinsicHash));
+
+        return block.FindExtrinsicIdxByHash(extrinsicHash)
+            ?? throw new Exception("TODO: Custom exception.");
+    }
+
+    /// <summary>
+    /// Returns the index of the extrinsic in the block if found, otherwise null.
+    /// </summary>
+    /// <param name="block"></param>
+    /// <param name="extrinsicHash"></param>
+    /// <returns></returns>
+    public static uint? FindExtrinsicIdxByHash(this Block block, Hash extrinsicHash)
+    {
+        for (var i = 0u; i < block.Extrinsics.Length; i++)
+        {
+            var extrinsic = block.Extrinsics[i];
+            var (_, extrinsicHashCalculated) = extrinsic.EncodeAndHash();
+            if (extrinsicHashCalculated.IsEqualTo(extrinsicHash))
+            {
+                return i;
+            }
+        }
+        return null;
+    }
+}
