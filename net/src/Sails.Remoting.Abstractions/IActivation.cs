08b089dcca273e9b9e833f23b4f4afce7a4a882b
Vadim Obradovich <vadik.on@gmail.com>
2024-11-01 18:50:15 +0100

    feat(net): add .NET abstractions, RemotingAction, RemotingReply (#623)

---
+++ net/src/Sails.Remoting.Abstractions/IActivation.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+using Substrate.Gear.Api.Generated.Model.gprimitives;
+
+namespace Sails.Remoting.Abstractions;
+
+public interface IActivation : IActionBuilder<IActivation>
+{
+    /// <summary>
+    /// Activates/creates a program from previously uploaded code
+    /// </summary>
+    /// <param name="codeId">Code identifier. This identifier can be obtained as a result of executing the gear.uploadCode extrinsic.</param>
+    /// <param name="salt">Salt bytes</param>
+    /// <param name="cancellationToken"></param>
+    /// <returns>Reply with Program identifier. <see cref="IReply{T}"/></returns>
+    Task<IReply<ActorId>> ActivateAsync(
+        CodeId codeId,
+        IReadOnlyCollection<byte> salt,
+        CancellationToken cancellationToken);
+}
---
+++ net/src/Sails.Remoting/DelegatingReply.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Sails.Remoting.Abstractions;
+using Sails.Remoting.Abstractions.Core;
+using Substrate.NetApi.Model.Types;
+
+namespace Sails.Remoting;
+
+public sealed class DelegatingReply<TResult, T>(RemotingReply<TResult> innerReply, Func<TResult, T> map) : IReply<T>
+    where T : IType, new()
+{
+    /// <inheritdoc />
+    ValueTask IAsyncDisposable.DisposeAsync() => innerReply.DisposeAsync();
+
+    /// <inheritdoc />
+    async Task<T> IReply<T>.ReceiveAsync(CancellationToken cancellationToken)
+    {
+        var result = await innerReply.ReadAsync(cancellationToken).ConfigureAwait(false);
+        return map(result);
+    }
+}
