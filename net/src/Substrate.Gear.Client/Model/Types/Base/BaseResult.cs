2add6b40ab9c1fcf2b0312fbdb48a48dba60d5bd
Vadim Obradovich <vadik.on@gmail.com>
2024-11-01 18:20:57 +0100

    feat(net): add missing .NET primitives (#624)

---
+++ net/src/Substrate.Gear.Client/Model/Types/Base/BaseResult.cs
@@ -0,0 +1,39 @@
+﻿using Substrate.NetApi.Model.Types;
+using Substrate.NetApi.Model.Types.Base;
+
+namespace Substrate.Gear.Client.Model.Types.Base;
+
+/// <summary>
+/// Result
+/// </summary>
+public enum BaseResult
+{
+
+    /// <summary>
+    /// >> Ok
+    /// </summary>
+    Ok = 0,
+
+    /// <summary>
+    /// >> Err
+    /// </summary>
+    Err = 1,
+}
+
+/// <summary>
+/// EnumResult
+/// </summary>
+public sealed class EnumBaseResult<T1, T2> : BaseEnumRust<BaseResult>
+    where T1 : IType, new()
+    where T2 : IType, new()
+{
+
+    /// <summary>
+    /// Initializes a new instance of the class.
+    /// </summary>
+    public EnumBaseResult()
+    {
+        this.AddTypeDecoder<T1>(BaseResult.Ok);
+        this.AddTypeDecoder<T2>(BaseResult.Err);
+    }
+}
