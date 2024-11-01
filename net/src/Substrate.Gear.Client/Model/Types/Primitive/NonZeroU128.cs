ea8923f3d16ffb5f0277302c6da85f7bb798f9c9
Vadim Obradovich <vadik.on@gmail.com>
2024-11-01 18:20:57 +0100

    feat(net): add missing .NET primitives (#624)

---
+++ net/src/Substrate.Gear.Client/Model/Types/Primitive/NonZeroU128.cs
@@ -0,0 +1,16 @@
+﻿using Substrate.Gear.Client.Model.Types.Base;
+using Substrate.NetApi.Attributes;
+using Substrate.NetApi.Model.Types.Metadata.Base;
+using Substrate.NetApi.Model.Types.Primitive;
+
+namespace Substrate.Gear.Client.Model.Types.Primitive;
+
+/// <summary>
+/// NonZeroU128
+/// </summary>
+[SubstrateNodeType(TypeDefEnum.Composite)]
+public sealed class NonZeroU128 : BaseNonZero<U128>
+{
+    /// <inheritdoc/>
+    public override string TypeName() => nameof(NonZeroU128);
+}
