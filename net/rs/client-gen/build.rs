0c29bdb065310adeb030c087113b5e2f2cf73d45
vobradovich <vadik.on@gmail.com>
2024-11-12 17:21:07 +0100

    remove csbindgen and native methods generation

--- net/rs/client-gen/build.rs
+++
@@ -1,8 +0,0 @@
-fn main() {
-    csbindgen::Builder::default()
-        .input_extern_file("src/lib.rs")
-        .csharp_dll_name("sails_net_client_gen")
-        .csharp_namespace("Sails.ClientGenerator")
-        .generate_csharp_file("../../src/Sails.ClientGenerator/NativeMethods.g.cs")
-        .unwrap();
-}
