d0a8778d3c9687ca81faa3d9f4e2945f8c8873d1
Dennis Diatlov <dennis@gear-tech.io>
2024-11-14 15:17:28 +0000

    tests(net): introduce helpers and fixtures for tests (#644)

---
+++ net/tests/Sails.Remoting.Tests/Core/RemotingViaNodeClientTests.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading.Tasks;
+using Sails.Remoting.Tests._Infra.XUnit.Fixtures;
+using Sails.Tests.Shared.XUnit;
+using Xunit;
+
+namespace Sails.Remoting.Tests.Core;
+
+public sealed class RemotingViaNodeClientTests : IAssemblyFixture<SailsFixture>
+{
+    public RemotingViaNodeClientTests(SailsFixture sailsFixture)
+    {
+        this.sailsFixture = sailsFixture;
+    }
+
+    private readonly SailsFixture sailsFixture;
+
+    [Fact]
+    public async Task Test()
+    {
+        var demoIdl = await this.sailsFixture.GetDemoContractIdlAsync();
+        var demoContractWasm = await this.sailsFixture.GetDemoContractWasmAsync();
+        var noSvcsProgIdl = await this.sailsFixture.GetNoSvcsProgContractIdlAsync();
+        var gearNodeWsUrl = this.sailsFixture.GearNodeWsUrl;
+    }
+}
---
+++ net/tests/Sails.Tests.Shared/XUnit/TestFrameworkExecutor.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections.Generic;
+using System.Reflection;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+namespace Sails.Tests.Shared.XUnit;
+
+public sealed class TestFrameworkExecutor : XunitTestFrameworkExecutor
+{
+    public TestFrameworkExecutor(
+        AssemblyName assemblyName,
+        ISourceInformationProvider sourceInformationProvider,
+        IMessageSink diagnosticMessageSink)
+        : base(assemblyName, sourceInformationProvider, diagnosticMessageSink)
+    {
+    }
+
+    protected override async void RunTestCases(
+        IEnumerable<IXunitTestCase> testCases,
+        IMessageSink executionMessageSink,
+        ITestFrameworkExecutionOptions executionOptions)
+    {
+        using (var assemblyRunner = new TestAssemblyRunner(
+                   this.TestAssembly,
+                   testCases,
+                   this.DiagnosticMessageSink,
+                   executionMessageSink,
+                   executionOptions))
+        {
+            await assemblyRunner.RunAsync();
+        }
+    }
+}
