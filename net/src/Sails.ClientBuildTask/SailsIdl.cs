8b16c94d24e5269a0330a7979022e57ababb3776
vobradovich <vadik.on@gmail.com>
2024-11-14 14:18:08 +0100

    wip: build task

---
+++ net/src/Sails.ClientBuildTask/SailsIdl.cs
@@ -0,0 +1,23 @@
+﻿#nullable disable
+using Microsoft.Build.Framework;
+using Microsoft.Build.Utilities;
+
+namespace Sails.ClientBuildTask;
+public class SailsIdl : Task
+{
+    //List of files
+    [Required]
+    public ITaskItem[] IdlFiles { get; set; }
+
+    //The name of the namespace where the class is going to be generated
+    public string NamespaceName { get; set; }
+
+    //The filename where the class was generated
+    [Output]
+    public string[] IdlGeneratedFiles { get; set; }
+
+    public override bool Execute()
+    {
+        return true;
+    }
+}
