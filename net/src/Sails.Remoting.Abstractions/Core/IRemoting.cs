222ab50dd4e07baae7b83e82f5bb7d0fab177b02
Dennis Diatlov <dennis@gear-tech.io>
2024-11-21 13:38:23 +0000

    [net]build: build sources from all projects as a single Sails.Net (#669)

--- net/src/Sails.Remoting.Abstractions/Core/IRemoting.cs
+++ net/src/Sails.Remoting.Abstractions/Core/IRemoting.cs
@@ -2,8 +2,6 @@
 using System.Threading;
 using System.Threading.Tasks;
 using Substrate.Gear.Api.Generated.Model.gprimitives;
-using GasUnit = Substrate.NetApi.Model.Types.Primitive.U64;
-using ValueUnit = Substrate.NetApi.Model.Types.Primitive.U128;
 
 namespace Sails.Remoting.Abstractions.Core;
 
