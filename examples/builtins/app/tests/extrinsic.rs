baa72737cc88fcd1fc16a18de5beb748d15a929c
Sabaun Taraki <sabaun.dev@gmail.com>
2025-07-22 18:51:07 +0300

    prepare for gas calc bug showcase #2

--- examples/builtins/app/tests/extrinsic.rs
+++ examples/builtins/app/tests/extrinsic.rs
@@ -8,30 +8,30 @@ use sails_rs::{
 
 const ONE_VARA: u128 = 1_000_000_000_000;
 
-#[tokio::test]
-async fn call_proxy_builtin_with_extrinsic() {
-    let api = GearApi::dev().await.unwrap();
+// #[tokio::test]
+// async fn call_proxy_builtin_with_extrinsic() {
+//     let api = GearApi::dev().await.unwrap();
 
-    let remoting = GClientRemoting::new(api.clone());
+//     let remoting = GClientRemoting::new(api.clone());
 
-    let proxy = ProxyBuiltin::new(remoting);
-    let random_actor_id = H256::random().into();
-    let res = proxy
-        .add_proxy(random_actor_id, SailsProxyType::Any)
-        .send_recv(PROXY_BUILTIN_ID)
-        .await
-        .expect("Failed to send proxy request");
+//     let proxy = ProxyBuiltin::new(remoting);
+//     let random_actor_id = H256::random().into();
+//     let res = proxy
+//         .add_proxy(random_actor_id, SailsProxyType::Any)
+//         .send_recv(PROXY_BUILTIN_ID)
+//         .await
+//         .expect("Failed to send proxy request");
 
-    assert_eq!(res, Vec::<u8>::new());
+//     assert_eq!(res, Vec::<u8>::new());
 
-    let res = proxy
-        .remove_proxy(random_actor_id, SailsProxyType::Any)
-        .send_recv(PROXY_BUILTIN_ID)
-        .await
-        .expect("Failed to send proxy request");
+//     let res = proxy
+//         .remove_proxy(random_actor_id, SailsProxyType::Any)
+//         .send_recv(PROXY_BUILTIN_ID)
+//         .await
+//         .expect("Failed to send proxy request");
 
-    assert_eq!(res, Vec::<u8>::new());
-}
+//     assert_eq!(res, Vec::<u8>::new());
+// }
 
 #[tokio::test]
 async fn call_staking_builtin_with_extrinsic() {
