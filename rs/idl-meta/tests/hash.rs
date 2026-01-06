afd54065415b794ebc9d8453186d2327e5f4ec81
vobradovich <vadik.on@gmail.com>
2026-01-07 00:25:48 +0100

    test: refine

--- rs/idl-meta/tests/hash.rs
+++ rs/idl-meta/tests/hash.rs
@@ -1,5 +1,3 @@
-use std::str::FromStr;
-
 use sails_idl_meta::*;
 
 mod fixture;
@@ -8,8 +6,5 @@ mod fixture;
 fn hash_counter() {
     let s = fixture::counter_service();
     let interface_id = s.interface_id().unwrap();
-    assert_eq!(
-        interface_id,
-        InterfaceId::from_str("0x579d6daba41b7d82").unwrap()
-    )
+    assert_eq!(interface_id, InterfaceId::from_u64(0x579d6daba41b7d82))
 }
