ccc39974661262eccec0d6e3916dba625c8d0bd8
Mansur Azatbek <mansur62624@gmail.com>
2026-01-21 12:09:32 +0500

    test: add override tests for ethexe selectors

--- rs/ethexe/ethapp/src/lib.rs
+++ rs/ethexe/ethapp/src/lib.rs
@@ -18,6 +18,46 @@ impl MyProgram {
     pub fn svc1(&self) -> SomeService {
         SomeService
     }
+
+    pub fn inherited(&self) -> InheritedService {
+        InheritedService::new()
+    }
+}
+
+pub struct BaseService;
+
+#[service]
+impl BaseService {
+    #[export]
+    pub fn foo(&self) -> u32 {
+        100
+    }
+}
+
+pub struct InheritedService {
+    base: BaseService,
+}
+
+impl InheritedService {
+    pub fn new() -> Self {
+        Self { base: BaseService }
+    }
+}
+
+impl From<InheritedService> for BaseService {
+    fn from(value: InheritedService) -> Self {
+        value.base
+    }
+}
+
+#[service(extends = BaseService)]
+impl InheritedService {
+    // We override 'foo' from BaseService using a method named 'bar'
+    #[override_entry(base_service_methods::Foo)]
+    #[export]
+    pub fn bar(&self) -> u32 {
+        200
+    }
 }
 
 pub struct SomeService;
