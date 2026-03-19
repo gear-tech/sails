401e7fb586ad1f250e765a202b448344d9c99dec
vobradovich <vadik.on@gmail.com>
2026-03-19 22:22:54 +0100

    fix

--- rs/src/types.rs
+++ rs/src/types.rs
@@ -2,6 +2,5 @@ pub use core::num::{NonZeroU8, NonZeroU16, NonZeroU32, NonZeroU64, NonZeroU128};
 pub use gprimitives::{ActorId, CodeId, H160, H256, MessageId, NonZeroU256, U256};
 pub use gsys::*;
 
-pub type ValueUnit = gsys::Value; // u64
-
-pub type GasUnit = gsys::Gas; // u128
+pub type ValueUnit = gsys::Value; // u128
+pub type GasUnit = gsys::Gas; // u64
